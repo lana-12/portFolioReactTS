98c5ee8fe3357615f921bb38e1812362ae418970
lana-12 <lana.12@wanadoo.fr>
2025-04-06 16:00:30 +0200

    Change section about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Vivi from '../img/Vivi.jpg';
 
-
 const About = () => {
     return (
         <>
@@ -10,30 +9,16 @@ const About = () => {
                 <img src={Vivi} alt="Vivi" className="img-fluid imgCV thumbnail " />
 
                 <p className="text-center pt-4">
-                Depuis mon enfance, je suis passionnée par l'informatique, fascinée par la technologie et animée par le désir de créer et d'innover. Cette passion m'a amenée à suivre une formation spécialisée en conception et développement web. Aujourd'hui, je vis mon rêve en programmant chaque jour avec enthousiasme et détermination, convaincue que l'informatique est bien plus qu'un métier, c'est une vocation.                
+                    Passionnée par l'informatique depuis toujours, j'ai orienté ma carrière vers le développement web et la data. Après une formation spécialisée, j'ai acquis une solide expertise en développement d'interfaces utilisateur, d'APIs, et en gestion de bases de données. Aujourd'hui, je m'épanouis dans des projets web et Big Data, convaincue que l'informatique est un domaine à la fois stimulant et d'impact.
                 </p>
-
 
                 <p className="text-center pt-4">
-                En tant que stagiaire chez Dexentri, pendant trois mois, j'ai eu l'opportunité captivante de prendre en charge la conception et le développement de l'interface utilisateur d'une plateforme de trading novatrice, spécialisée dans les bouteilles de vin. Ce projet m'a plongé au cœur de technologies de pointe, notamment React pour l'interface utilisateur, ainsi que des requêtes en NoSQL sur MastermindCMS et en SQL sur une API Rest. Cette expérience enrichissante m'a permis d'approfondir mes compétences en développement web tout en travaillant sur un projet concret et stimulant.              
+                    Lors de mon stage chez Dexentri, j'ai travaillé sur la conception de l'interface d'une plateforme de trading, utilisant React, NoSQL, et API REST. Cette expérience m'a permis d'approfondir mes compétences techniques tout en contribuant à un projet concret et innovant.
                 </p>
 
-
-                {/* <p className="textIndent">
-                    Aujourd'hui, je vis mon rêve en programmant chaque jour avec enthousiasme et détermination. Je suis convaincue que l'informatique est bien plus qu'un simple métier, c'est une passion qui me pousse à me lever chaque matin avec le désir de créer, d'innover et de contribuer au monde numérique.
+                <p className="text-center pt-4">
+                    Aujourd'hui, je développe des solutions sur des projets comme Canaljob, où je gère la plateforme et les flux de données, tout en continuant à évoluer avec des technologies comme Symfony, React, Python et Big Data.
                 </p>
-                <p className="textIndent">
-                    Ma passion pour l'informatique a commencé dès mon plus jeune âge lorsque j'ai découvert le monde fascinant de la technologie. Même lorsque mes parents doutaient de l'avenir de ce domaine, j'étais intriguée par chaque nouvel appareil et interface intuitive.
-                </p>
-
-                <p className="textIndent">
-                    Avec le temps, j'ai réalisé que mon intérêt pour l'informatique n'était pas une simple phase, mais bien une véritable passion.
-                    Convaincue de ma vocation, j'ai décidé de me former dans le domaine du développement web et web mobile en suivant une formation spécialisée.
-                </p>
-                <p className="textIndent">
-                    J'ai ensuite enchaîné avec une autre formation pour enrichir mes connaissances et approfondir mes compétences dans le domaine.
-                    Cette décision représentait pour moi bien plus qu'un simple choix de carrière, mais une véritable affirmation de ma vocation dans le monde de la technologie.
-                </p> */}
             </div>
         </div>
         </>
@@ -41,5 +26,3 @@ const About = () => {
 }
 
 export default About;
-
-
